9c1d900cdc583b597c6085eddfaf097a57c4ca15
damonryon <damon.ryon@gmail.com>
2024-08-14 16:26:00 -0700

    question 1 functional

---
+++ index.js
@@ -0,0 +1,25 @@
+const rs = require('readline-sync')
+
+
+let arr = []
+let operation
+let operators = "/*-+"
+
+const calculator = () => {
+  operation = rs.question('What operation would you like to perform?')
+  if (operators.includes(operation)) {
+    arr.push(operation)
+  } else {
+    badOperator()
+  }
+}
+
+
+
+const badOperator = () => {
+  console.log("That is not a valid operation.")
+  calculator()
+}
+
+
+calculator()
